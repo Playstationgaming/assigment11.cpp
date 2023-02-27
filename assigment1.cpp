79df96bea50dd7f32a38f1a60f14c78d0dc625c3
Raj chaturvedi <116450136+Playstationgaming@users.noreply.github.com>
2023-02-27 12:33:18 +0530

    Update assigment1.cpp

--- assigment1.cpp
+++ assigment1.cpp
@@ -9,14 +9,14 @@ bool is_perfect_matrix(vector<vector<int>>& matrix) {
     int ref_element = 0;
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            // Check if the current element is on the top-left to bottom-right diagonal
+           
             if (i == j) {
-                // Check if this is the first diagonal element
+
                 if (j == 0) {
-                    // Store the diagonal element as the reference element
+               
                     ref_element = matrix[i][j];
                 } else {
-                    // Check if the current diagonal element is the same as the reference element
+                 
                     if (matrix[i][j] != ref_element) {
                         return false;
                     }
@@ -28,7 +28,7 @@ bool is_perfect_matrix(vector<vector<int>>& matrix) {
 }
 
 int main() {
-    // Test the is_perfect_matrix function with some example matrices
+    
     vector<vector<int>> matrix1 = {{1, 2, 3}, {4, 1, 2}, {5, 4, 1}};
     vector<vector<int>> matrix2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     vector<vector<int>> matrix3 = {{1, 2}, {3, 1}, {4, 3}};
